54a41d99a76ae5661f125f7223915530562745df
jmhupanen <juho.hupanen@hotmail.fi>
2023-03-11 12:50:05 +0200

    Use axios instead of fetch API

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -7,27 +8,18 @@ const router = express.Router();
 
 
 router.get('/switchState', async (req, res) => {
-  let json;
-
   try {
-    const response = await fetch(process.env.APPS_SCRIPT_URL, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ API: process.env.APPS_SCRIPT_API_KEY })
-    });
-    json = await response.json();
+    const payload = { API: process.env.APPS_SCRIPT_API_KEY }
+    const response = await axios.post(process.env.APPS_SCRIPT_URL, payload);
+    console.log(response.data);
+    if ('switchState' in response.data) {
+      res.status(200).send(response.data);
+    } else {
+      res.status(400).send('Key invalid');
+    }
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
-  }
-
-  if (json && 'switchState' in json) {
-    res.status(200).send(json);
-  } else {
-    res.status(400).send('Key invalid');
   }
 });
 
